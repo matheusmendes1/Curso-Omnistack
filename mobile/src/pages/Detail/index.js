21c0c39b0bd41267e173474ccebde4a7c321b8d4
Matheus Santos <matheusmendes1@hotmail.com>
2020-03-28 20:11:05 -0300

    Mobile - hour 01:07:30

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,9 +10,22 @@ import styles from './styles';
 
 export default function Detail () {
     const navigation = useNavigation();
+    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" no valor de R$120.00';
 
     function navigateBack() {
         navigation.goBack();
+    }
+
+    function sendMail() {
+        MailComposer.composeAsync({ 
+            subject: 'Herói do Caso: Cadelinha Atropelada',
+            recipients: ['matheusmendes1@hotmail.com'],
+            body: message
+        })
+    }
+
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=55034988312566&text=${message}`);
     }
 
     return (
@@ -42,11 +56,11 @@ export default function Detail () {
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={ sendWhatsapp }>
                         <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={ sendMail }>
                         <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
